881a7761b4d51403284cb7348c5e005d7a65c7a3
Emmanuel Maximus Yohanes <geofanny.emmanuel@gmail.com>
2020-11-11 22:01:51 +0700

    changed datatype for password and email

--- Models/user.cs
+++ Models/user.cs
@@ -11,7 +11,8 @@ namespace webpro_quiz2.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class user
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -21,8 +22,13 @@ namespace webpro_quiz2.Models
         }
     
         public int user_id { get; set; }
+
+        [DataType(DataType.EmailAddress)]
         public string email { get; set; }
+
+        [DataType(DataType.Password)]
         public string password { get; set; }
+
         public string fullname { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
