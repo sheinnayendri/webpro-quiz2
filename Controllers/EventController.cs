65a2a63b824c9cb88fa9ab39020e5afeb8769f2b
heizzz <gautamabryan@gmail.com>
2020-11-12 09:44:55 +0800

    CRUD event

--- Controllers/EventController.cs
+++ Controllers/EventController.cs
@@ -29,6 +29,47 @@ namespace webpro_quiz2.Controllers
             }
                 
         }
+        
+        public ActionResult EditEvent(int id)
+        {
+            using (EventsCoEntities db = new EventsCoEntities())
+            {
+                var data = db.events.Where(x => x.event_id == id).SingleOrDefault();
+                return View();
+            }
+                
+        }
+
+        [HttpPost]
+        public ActionResult EditEvent(int id, @event ev)
+        {
+            using (EventsCoEntities db = new EventsCoEntities())
+            {
+                var e = (from a in db.events where a.event_id == id select a).FirstOrDefault();
+                e.event_name = ev.event_name;
+                e.event_place = ev.event_place;
+                e.event_price = ev.event_price;
+                e.event_start_date = ev.event_start_date;
+                e.event_start_time = ev.event_start_time;
+                e.event_end_date = ev.event_end_date;
+                e.event_end_time = ev.event_end_time;
+                db.SaveChanges();
+            }
+            return RedirectToAction("Dashboard", "User", new { area = "" });
+        }
+
+        public ActionResult DeleteEvent(int id)
+        {
+            using (EventsCoEntities db = new EventsCoEntities())
+            {
+                var data = db.events.FirstOrDefault(x => x.event_id == id);
+                    db.events.Remove(data);
+                    db.SaveChanges();
+                    return RedirectToAction("Dashboard", "User", new { area = "" });
+            }
+        }
+
+
     }
 
 }
